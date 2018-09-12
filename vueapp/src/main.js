ee228322b3be4c976db7591dd3ce15476f7ce6e0
wyy789 <948174330@qq.com>
2018-09-12 16:36:17 +0800

    路由上传

--- vueapp/src/main.js
+++ vueapp/src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+// import login from "./components/login/login"
+// Vue.component("login", login)
+
 Vue.config.productionTip = false
 
 new Vue({
