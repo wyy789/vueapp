21aeb98fad31012d94c794537900e9ec2818732b
LiuXin5678 <263716686@qq.com>
2018-09-13 10:40:28 +0800

    我要拉取，所以提交

--- vueapp/src/router.js
+++ vueapp/src/router.js
@@ -39,7 +39,7 @@ export default new Router({
     {
       path: '/spoilmanagement',
       name: 'spoilmanagement',
-      component: spoilmanagement
+      component: spoilmanagementn
     },
     {
       path: '/storeapplication',
